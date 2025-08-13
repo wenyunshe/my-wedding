4d2cdd6edc56eaf5fe5dfafebfce323ec0378ea7
Wenyun She <wehuin1215@gmail.com>
2025-08-13 22:58:07 +0800

    Add theme.css and hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { PrimeReactProvider } from 'primereact/api'
-import { Menubar } from 'primereact/menubar'
-import 'primereact/resources/themes/fluent-light/theme.css'
+// import { Menubar } from 'primereact/menubar'
+import './assets/theme.css'
 import { Outlet } from 'react-router'
 
 function App() {
   return (
     <PrimeReactProvider>
-      <Menubar
+      {/* <Menubar
         model={[
           { label: 'Home', url: '/' },
           { label: 'Our story', url: '/our-story' },
           { label: 'Gallery', url: '/gallery' },
         ]}
-      />
+      /> */}
       <div className='bg-[#f0E9E5] min-h-screen'>
         <Outlet />
       </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,7 @@
 const HomePage = () => {
   return (
-    <div className='flex py-16 md:py-40 px-6 justify-center items-center flex-col gap-5'>
-      <h1 className='text-3xl md:text-7xl font-hand font-bold text-center text-[#443122]'>
-        Hengtong & Wenyun
-      </h1>
+    <div className='flex py-16 md:py-28 px-6 justify-center items-center flex-col gap-5'>
+      <img src='public/hero.svg' />
       <h4 className='text-xl md:text-2xl text-center'>
         You are invited to the wedding
       </h4>
@@ -15,7 +13,10 @@ const HomePage = () => {
       >
         點我加入 Google 行事曆
       </a>
-      <h3>2026/3/28 @ 高雄日航酒店</h3>
+      <h3 className='text-ms/8 md:text-xl/9 text-center'>
+        2026/3/28
+        <br />@ 高雄日航酒店
+      </h3>
       <iframe
         className='size-72'
         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.1765795558445!2d120.30279087564934!3d22.609880331602284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e030004dc9165%3A0xf1c8ab02b634069b!2z6auY6ZuE5pel6Iiq6YWS5bqXIEhvdGVsIE5pa2tvIEthb2hzaXVuZw!5e0!3m2!1szh-TW!2stw!4v1751132070968!5m2!1szh-TW!2stw'
