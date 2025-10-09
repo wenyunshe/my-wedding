e239d80fd49384c3b8a59e49c2e76bac255b8615
Wenyun She <wehuin1215@gmail.com>
2025-10-09 17:41:03 +0800

    Add homepage animation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,56 @@
+import { motion } from 'motion/react'
+
+const fadeInProps = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+}
+
 const HomePage = () => {
   return (
-    <div className='flex py-16 md:py-14 px-6 justify-center items-center flex-col gap-5'>
-      <img src='/hero.svg' className='w-full md:w-2xl' />
-      <h4 className='text-xl md:text-2xl text-center'>
+    <div className='flex py-8 md:py-14 px-6 justify-center items-center flex-col gap-5'>
+      <motion.img
+        src='/hero.svg'
+        className='w-full md:w-2xl'
+        {...fadeInProps}
+        transition={{ duration: 0.5 }}
+      />
+      <motion.h4
+        className='text-xl md:text-2xl text-center'
+        {...fadeInProps}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
         You are invited to the wedding
-      </h4>
-      <h3 className='text-ms/8 md:text-xl/9 text-center'>
+      </motion.h4>
+      <motion.h3
+        className='text-ms/8 md:text-xl/9 text-center'
+        {...fadeInProps}
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
         <span className='text-2xl/10 md:text-3xl/12'>
           2026/3/28&nbsp;&nbsp;&nbsp;12:30 PM<small>（午宴）</small>
         </span>
         <br />
         <span className='pi pi-at' />
         &nbsp;高雄日航酒店
-      </h3>
-      <a
+      </motion.h3>
+      <motion.a
         href={import.meta.env.VITE_CALENDER_EVENT_URL}
         target='_blank'
         rel='noopener noreferrer'
         className='p-button p-button-outlined'
+        {...fadeInProps}
+        transition={{ duration: 0.5, delay: 0.6 }}
       >
         點我加入 Google 行事曆
-      </a>
-      <iframe
+      </motion.a>
+      <motion.iframe
         className='size-72'
         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.1765795558445!2d120.30279087564934!3d22.609880331602284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e030004dc9165%3A0xf1c8ab02b634069b!2z6auY6ZuE5pel6Iiq6YWS5bqXIEhvdGVsIE5pa2tvIEthb2hzaXVuZw!5e0!3m2!1szh-TW!2stw!4v1751132070968!5m2!1szh-TW!2stw'
         loading='lazy'
         referrerPolicy='no-referrer-when-downgrade'
-      ></iframe>
+        {...fadeInProps}
+        transition={{ duration: 0.5, delay: 0.8 }}
+      ></motion.iframe>
     </div>
   )
 }
