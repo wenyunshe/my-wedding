8d6df0121765df3bce75cbd05ac8ba8c68bf1e27
Wenyun She <wehuin1215@gmail.com>
2025-12-09 22:10:18 +0800

    Tweak homepage layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -32,19 +32,10 @@ const HomePage = () => {
         >
           You are invited to the wedding!
         </motion.h4>
-        <motion.a
-          href='/rsvp'
-          rel='noopener noreferrer'
-          className='p-button'
-          {...fadeInProps}
-          transition={{ duration: 0.5, delay: 0.3 }}
-        >
-          填寫 RSVP
-        </motion.a>
         <motion.h3
           className='text-ms/8 md:text-xl/9'
           {...fadeInProps}
-          transition={{ duration: 0.5, delay: 0.4 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
         >
           <span className='text-2xl/10 md:text-3xl/12'>
             2026/3/28&nbsp;&nbsp;&nbsp;12:30 PM<small>（午宴）</small>
@@ -53,16 +44,23 @@ const HomePage = () => {
           <span className='pi pi-at' />
           &nbsp;高雄日航酒店
         </motion.h3>
-        <motion.a
-          href={import.meta.env.VITE_CALENDER_EVENT_URL}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='p-button p-button-outlined'
+        <motion.div
+          className='flex gap-3'
           {...fadeInProps}
-          transition={{ duration: 0.5, delay: 0.5 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
         >
-          點我加入 Google 行事曆
-        </motion.a>
+          <a href='/rsvp' rel='noopener noreferrer' className='p-button'>
+            填寫 RSVP
+          </a>
+          <a
+            href={import.meta.env.VITE_CALENDER_EVENT_URL}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='p-button p-button-outlined'
+          >
+            點我加入 Google 行事曆
+          </a>
+        </motion.div>
         <motion.iframe
           className='lg:size-120 md:size-100 size-80 rounded-lg shadow-lg border-0'
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.1765795558445!2d120.30279087564934!3d22.609880331602284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e030004dc9165%3A0xf1c8ab02b634069b!2z6auY6ZuE5pel6Iiq6YWS5bqXIEhvdGVsIE5pa2tvIEthb2hzaXVuZw!5e0!3m2!1szh-TW!2stw!4v1751132070968!5m2!1szh-TW!2stw'
